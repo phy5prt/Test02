da6f4f650cba5e48ad13f0173ca97782c3d8f85a
phy5prt <philip.r.tate@gmail.com>
2018-10-06 07:38:06 +0100

    making a commitment

--- Assets/script/testscript.cs
+++ Assets/script/testscript.cs
@@ -6,7 +6,9 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+	print("lalalalallala");
+	print ("something changed");
 	}
 	
 	// Update is called once per frame
